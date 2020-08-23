858fd6708282300388cce3610bde7477f1bfc6bb
vyvian <vyvian.nellira@gmail.com>
2020-08-23 16:20:57 +0200

    initial setup

---
+++ flaskApp.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+
+UPLOAD_FOLDER ='https://patw1h5276.execute-api.eu-west-1.amazonaws.com/beta/upload'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+
+app.config["FILE_UPLOADS"] = UPLOAD_FOLDER
+# app.config['MAX_CONTENT_PATH'] =
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return render_template("index.html")
+
+@app.route('/uploader', methods = ['Get', 'POST'])
+def uploadFile():
+	if request.method == 'POST':
+		f = request.files['file']
+		f.save(secure_filename(f.filename))
+		return ' File was uploaded successfully'
+	return render_template("uploadFile.html")
+
+if __name__ == '__main__':
+	app.run(debug = True)
